8e2c61a2a1e905b302de6a522614e0061b23a3f1
nagendra.yadav.j2ee <nagendra.yadav.j2ee@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-11 23:32:04 +0000

    Spring and Hibernate Integration

---
+++ synergy-bank/src/main/java/com/synergy/bank/base/dao/AbstractDao.java
@@ -0,0 +1,21 @@
+package com.synergy.bank.base.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.criterion.Criterion;
+
+/**
+ * @author nagendra
+ * @param <E>
+ * @param <I>
+ */
+public interface AbstractDao<E, I extends Serializable> {
+
+	public E findById(I id);
+    public void saveOrUpdate(E e);
+    public void delete(E e);
+    public List<E> findByCriteria(Criterion criterion);
+    public List<E> findAll();
+    
+}
---
+++ synergy-bank/src/main/java/com/synergy/bank/base/dao/AbstractDaoImpl.java
@@ -0,0 +1,61 @@
+package com.synergy.bank.base.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+
+/**
+ * @author yadna01
+ * @param <E>
+ * @param <I>
+ */
+public abstract class AbstractDaoImpl<E, I extends Serializable> implements AbstractDao<E,I> {
+
+    private Class<E> entityClass;
+
+    protected AbstractDaoImpl(Class<E> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    @Autowired
+    @Qualifier("synegySessionFactory")
+    private SessionFactory psessionFactory;
+
+    public Session getCurrentSession() {
+        return psessionFactory.getCurrentSession();
+    }
+
+    @Override
+    public E findById(I id) {
+        return (E) getCurrentSession().get(entityClass, id);
+    }
+
+    @Override
+    public void saveOrUpdate(E e) {
+        getCurrentSession().saveOrUpdate(e);
+    }
+
+    @Override
+    public void delete(E e) {
+        getCurrentSession().delete(e);
+    }
+
+    @Override
+    public List<E> findByCriteria(Criterion criterion) {
+        Criteria criteria = getCurrentSession().createCriteria(entityClass);
+        criteria.add(criterion);
+        return criteria.list();
+    }
+    
+    @Override
+    public List<E> findAll() {
+    	return getCurrentSession().createQuery("from "+entityClass.getName()).list();
+    }
+}
