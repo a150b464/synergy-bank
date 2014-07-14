6505e87d66b57e8497e22616f4aa439e8de10095
nagendra.yadav.j2ee <nagendra.yadav.j2ee@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-14 18:51:06 +0000

    Bank Login

--- synergy-bank/src/main/java/com/synergy/bank/customer/web/constant/NavigationConstant.java
+++ synergy-bank/src/main/java/com/synergy/bank/customer/web/constant/NavigationConstant.java
@@ -7,10 +7,13 @@ package com.synergy.bank.customer.web.constant;
  */
 public interface NavigationConstant {
 	
+	final public static String COMMON_PAGE="WEB-INF/jsp/common/";
 	final public static String CUSTOMER_PAGE="WEB-INF/jsp/customer/";
 	final public static String CUSTOMER_REGISTRATION_PAGE="customerRegistration";
 	final public static String CUSTOMER_PAYEE_LIST_PAGE="payeeList";
 	final public static String ADD_PAYEE_PAGE="payeeDetailsSynergyBank";
+	final public static String LOGIN_PAGE="login";
+	final public static String CUSTOMER_HOME_PAGE="customerHome";
 	final public static String FUND_TRANSFER_PAGE="fundTransfer";
 	final public static String MAKE_PAYMENT_PAGE="makePayment";
 }
