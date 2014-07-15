30b8ce74f8b20a8683e58034351c90fe02b96481
jeffcng@gmail.com <jeffcng@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-15 22:51:52 +0000

    Account Summary

--- synergy-bank/src/main/java/com/synergy/bank/customer/dao/query/CustomerQuery.java
+++ synergy-bank/src/main/java/com/synergy/bank/customer/dao/query/CustomerQuery.java
@@ -1,6 +1,5 @@
 package com.synergy.bank.customer.dao.query;
 
-import java.util.Date;
 
 /**
  * 
@@ -8,10 +7,11 @@ import java.util.Date;
  *
  */
 public interface CustomerQuery {
- public static final String INSERT_CUSTOMER="insert into customer_details_tbl(userId,password,salutation,gender,firstName,middleName,lastName,maritalStatus,dob,category,motherMaidenName,fatherName,email,mobile,ssn,occupation,occupationType,education,grossAnualIncome,sourceOfFunds,role,photo,doe,dom,description) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
- public static final String FIND_CUTOMER ="select * from customer_details_tbl";
- public static final String SHOW_PART_PAYEELIST ="select * from payee_information_tbl where userid=";
- public static final String SHOW_PAYEE_LIST="select * from payee_information_tbl where userid=";
- public static final String ADD_PAYEE="insert into payee_information_tbl(sno,userid,payeeAccountNo,payeeName,payeeNickName,mobile,doe,email,status) values(?,?,?,?,?,?,?,?,?)";
- public static final String APPROVE_PAYEE_STATUS="update payee_information_tbl set status='approved' where userid=";
+	public static final String INSERT_CUSTOMER = "insert into customer_details_tbl(userId,password,salutation,gender,firstName,middleName,lastName,maritalStatus,dob,category,motherMaidenName,fatherName,email,mobile,ssn,occupation,occupationType,education,grossAnualIncome,sourceOfFunds,role,photo,doe,dom,description) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	public static final String FIND_CUTOMER = "select * from customer_details_tbl";
+	public static final String SHOW_PART_PAYEELIST = "select * from payee_information_tbl where userid=";
+	public static final String SHOW_PAYEE_LIST = "select * from payee_information_tbl where userid=";
+	public static final String ADD_PAYEE = "insert into payee_information_tbl(sno,userid,payeeAccountNo,payeeName,payeeNickName,mobile,doe,email,status) values(?,?,?,?,?,?,?,?,?)";
+	public static final String APPROVE_PAYEE_STATUS = "update payee_information_tbl set status='approved' where userid=";
+	public static final String FIND_CUSTOMER_ACCOUNT_BY_ID = "select * from customer_account_info_tbl where userid=";
 }
