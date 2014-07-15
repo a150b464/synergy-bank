3ee1569500de4af021bfd57242d080f9ac8b0f08
jeffcng@gmail.com <jeffcng@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-15 22:53:27 +0000

    Account Summary

--- synergy-bank/src/main/java/com/synergy/bank/customer/web/controller/form/CustomerAccountForm.java
+++ synergy-bank/src/main/java/com/synergy/bank/customer/web/controller/form/CustomerAccountForm.java
@@ -3,8 +3,7 @@ package com.synergy.bank.customer.web.controller.form;
 import java.util.Date;
 
 public class CustomerAccountForm {
-	
-	private int id;
+	// private int id;
 	private String userid;
 	private String customerName;
 	private String customerAccountNo;
@@ -14,83 +13,91 @@ public class CustomerAccountForm {
 	private String currency;
 	private Date doe;
 	private Date dom;
-	private String customerEmail;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
+	/*
+	 * public int getId() { return id; }
+	 * 
+	 * public void setId(int id) { this.id = id; }
+	 */
 	public String getUserid() {
 		return userid;
 	}
+
 	public void setUserid(String userid) {
 		this.userid = userid;
 	}
+
 	public String getCustomerName() {
 		return customerName;
 	}
+
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
+
 	public String getCustomerAccountNo() {
 		return customerAccountNo;
 	}
+
 	public void setCustomerAccountNo(String customerAccountNo) {
 		this.customerAccountNo = customerAccountNo;
 	}
+
 	public String getAccountType() {
 		return accountType;
 	}
+
 	public void setAccountType(String accountType) {
 		this.accountType = accountType;
 	}
+
 	public Double getAvailBalance() {
 		return availBalance;
 	}
+
 	public void setAvailBalance(Double availBalance) {
 		this.availBalance = availBalance;
 	}
+
 	public Double getTotalAvailBalance() {
 		return totalAvailBalance;
 	}
+
 	public void setTotalAvailBalance(Double totalAvailBalance) {
 		this.totalAvailBalance = totalAvailBalance;
 	}
+
 	public String getCurrency() {
 		return currency;
 	}
+
 	public void setCurrency(String currency) {
 		this.currency = currency;
 	}
+
 	public Date getDoe() {
 		return doe;
 	}
+
 	public void setDoe(Date doe) {
 		this.doe = doe;
 	}
+
 	public Date getDom() {
 		return dom;
 	}
+
 	public void setDom(Date dom) {
 		this.dom = dom;
 	}
-	
-	public String getCustomerEmail() {
-		return customerEmail;
-	}
-	public void setCustomerEmail(String customerEmail) {
-		this.customerEmail = customerEmail;
-	}
+
 	@Override
 	public String toString() {
-		return "CustomerAccountForm [id=" + id + ", userid=" + userid
-				+ ", customerName=" + customerName + ", customerAccountNo="
-				+ customerAccountNo + ", accountType=" + accountType
-				+ ", availBalance=" + availBalance + ", totalAvailBalance="
-				+ totalAvailBalance + ", currency=" + currency + ", doe=" + doe
-				+ ", dom=" + dom + ", customerEmail="+ customerEmail+"]";
+		return "CustomerAccountForm [ userid=" + userid + ", customerName="
+				+ customerName + ", customerAccountNo=" + customerAccountNo
+				+ ", accountType=" + accountType + ", availBalance="
+				+ availBalance + ", totalAvailBalance=" + totalAvailBalance
+				+ ", currency=" + currency + ", doe=" + doe + ", dom=" + dom
+				+ "]";
 	}
-	
 }
