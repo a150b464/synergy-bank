661d8416b97bb96993f4579c30716bf3e846090d
jeffcng@gmail.com <jeffcng@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-15 22:33:47 +0000

    Account Summary

---
+++ synergy-bank/src/main/java/com/synergy/bank/customer/dao/impl/CustomerAccountDaoImpl.java
@@ -0,0 +1,37 @@
+package com.synergy.bank.customer.dao.impl;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import com.synergy.bank.customer.dao.CustomerAccountDao;
+import com.synergy.bank.customer.dao.entity.CustomerAccountEntity;
+import com.synergy.bank.customer.dao.query.CustomerQuery;
+
+@Repository("CustomerAccountDaoImpl")
+@Scope("singleton")
+public class CustomerAccountDaoImpl extends JdbcDaoSupport implements
+		CustomerAccountDao {
+
+	@Autowired
+	@Qualifier("bankDataSource")
+	public void setDataSourceInBank(DataSource dataSource) {
+		super.setDataSource(dataSource);
+	}
+
+	public List<CustomerAccountEntity> findCustomerAccountByUserId(String userId) {
+		List<CustomerAccountEntity> accountList = super.getJdbcTemplate()
+				.query(CustomerQuery.FIND_CUSTOMER_ACCOUNT_BY_ID + "'" + userId
+						+ "'",
+						new BeanPropertyRowMapper<CustomerAccountEntity>(
+								CustomerAccountEntity.class));
+		return accountList;
+	}
+}
