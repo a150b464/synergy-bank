dd8dff414dc4026b3e1de8e401bcd692140f0f3d
tridib04@gmail.com <tridib04@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-16 17:01:14 +0000

    Payee module

--- synergy-bank/src/main/java/com/synergy/bank/customer/service/BankPayeeService.java
+++ synergy-bank/src/main/java/com/synergy/bank/customer/service/BankPayeeService.java
@@ -8,14 +8,15 @@ package com.synergy.bank.customer.service;
 
 import java.util.List;
 
-import com.synergy.bank.customer.dao.entity.PayeeDetailsEntity;
+import com.synergy.bank.customer.web.controller.form.PayeeDetailsForm;
 import com.synergy.bank.customer.web.controller.form.CustomerForm;
 import com.synergy.bank.customer.web.controller.form.PayeeDetailsForm;
 
 public interface BankPayeeService {
 	
 	public String addPayee(PayeeDetailsForm payeeDetailsForm);
-	public String confirmPayee(String payeeAccountNo, String userId);	
+	public String confirmPayee(PayeeDetailsForm payeeDetailsForm);
+	public List<PayeeDetailsForm> getPayeeListForUserId(String userId);
 	public List<PayeeDetailsForm> findPayeeByUserId(String userid);
 	public boolean isPayeeExists(String userId, String payeeAccountNo);
 }
