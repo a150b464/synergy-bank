1d2650c1d966f4d090d3909016dada1cba0a8ec6
tridib04@gmail.com <tridib04@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-16 16:55:49 +0000

    Payee module

--- synergy-bank/src/main/java/com/synergy/bank/customer/web/controller/BankPayeeCustomerController.java
+++ synergy-bank/src/main/java/com/synergy/bank/customer/web/controller/BankPayeeCustomerController.java
@@ -1,4 +1,5 @@
 package com.synergy.bank.customer.web.controller;
+
 import javax.servlet.http.HttpSession;
 
 import java.util.Date;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.synergy.bank.customer.service.BankPayeeService;
 import com.synergy.bank.customer.web.constant.NavigationConstant;
-import com.synergy.bank.customer.web.controller.form.CustomerTransactionForm;
+/*import com.synergy.bank.customer.web.controller.form.CustomerTransactionForm;*/
 import com.synergy.bank.customer.web.controller.form.PayeeDetailsForm;
 
 @Controller
@@ -25,63 +26,61 @@ public class BankPayeeCustomerController {
 	@Autowired
 	@Qualifier("BankPayeeServiceImpl")
 	private BankPayeeService bankPayeeService;
-	
-	
+
 	// Add Payee Methods
 	@RequestMapping(value = "addpayee.do", method = RequestMethod.GET)
 	public String addpayee(Model model) {
 		PayeeDetailsForm payeeDetailsForm = new PayeeDetailsForm();
 		model.addAttribute("addPayeeCommand", payeeDetailsForm);
 		System.out.println("in controller");
-		return NavigationConstant.CUSTOMER_PAGE+NavigationConstant.ADD_PAYEE_PAGE;
+		return NavigationConstant.CUSTOMER_PAGE
+				+ NavigationConstant.ADD_PAYEE_PAGE;
 	}
-		
-	
+
 	@RequestMapping(value = "addpayee.do", method = RequestMethod.POST)
-	public String addpayee(@ModelAttribute("addPayeeCommand")PayeeDetailsForm payeeDetailsForm) {
-		System.out.println("in post");
-		String id = "11";
-		String stst = "pending";
-		payeeDetailsForm.setDoe(new Date());
-		payeeDetailsForm.setUserid(id);
-		payeeDetailsForm.setStatus(stst);
-		bankPayeeService.addPayee(payeeDetailsForm);
-		return NavigationConstant.CUSTOMER_PAGE+NavigationConstant.CONFIRM_PAYEE_PAGE;
-	}
-	
-/*	// Confirm Payee Methods
-	@RequestMapping(value = "confirmpayee.do", method = RequestMethod.GET)
-	public String confirmpayee(Model model) {
-		PayeeDetailsForm payeeDetailsForm = new PayeeDetailsForm();
-		model.addAttribute("addPayeeCommand", payeeDetailsForm);
-		System.out.println("in controller");
-		return NavigationConstant.CUSTOMER_PAGE+NavigationConstant.CONFIRM_PAYEE_PAGE;
-	}
-	*/
-	@RequestMapping(value = "confirmpayee.do", method = RequestMethod.POST)
-	public String confirmpayee(@ModelAttribute("addPayeeCommand")PayeeDetailsForm payeeDetailsForm) {
-		System.out.println("in post");
-		bankPayeeService.addPayee(payeeDetailsForm);
-		return NavigationConstant.CUSTOMER_PAGE+NavigationConstant.ADD_PAYEE_PAGE;
-	}
-	
-	@RequestMapping(value="selectPayee",method=RequestMethod.GET) 
-	public String selectPayee(/*@RequestParam("userId") String userId,*/Model model) {
-		
-		CustomerTransactionForm customerTransactionCommand = new CustomerTransactionForm();
-		model.addAttribute("customerTransactionCommand",customerTransactionCommand);
-		return NavigationConstant.CUSTOMER_PAGE+NavigationConstant.FUND_TRANSFER_PAGE;
-	}
-	
-	@ModelAttribute("payeeDetailsFormList")
-	public Map<String,String> findPayeeListCustomer(){
-		List<PayeeDetailsForm>payeeDetailsFormList = bankPayeeService.getPayeeListForUserId("1");
-		LinkedHashMap<String,String> payeeList=new LinkedHashMap<String, String>();
-		for(PayeeDetailsForm detailsForm:payeeDetailsFormList){
-			payeeList.put(detailsForm.getUserid(), detailsForm.getPayeeName() +" - "+detailsForm.getUserid());
+	public String addpayee(
+			@ModelAttribute("addPayeeCommand") PayeeDetailsForm payeeDetailsForm,
+			Model model) {
+
+		String userId = "admin";
+		if (bankPayeeService.isPayeeExists(userId,
+				payeeDetailsForm.getPayeeAccountNo())) {
+			String message = "This payee already exists.";
+			model.addAttribute("message", message);
+			return NavigationConstant.CUSTOMER_PAGE
+					+ NavigationConstant.ADD_PAYEE_PAGE;
+		} else {
+			String stst = "pending";
+			payeeDetailsForm.setDoe(new Date());
+			payeeDetailsForm.setUserid(userId);
+			payeeDetailsForm.setStatus(stst);
+			bankPayeeService.addPayee(payeeDetailsForm);
+			model.addAttribute("payeeDetailsForm", payeeDetailsForm);
+			return NavigationConstant.CUSTOMER_PAGE
+					+ NavigationConstant.CONFIRM_PAYEE_PAGE;
 		}
-		return payeeList;
-		
+
 	}
-	
+
+	@RequestMapping(value = "confirmpayee.do", method = RequestMethod.POST)
+	public String confirmpayee(
+			@ModelAttribute("confirmPayeeCommand") PayeeDetailsForm payeeDetailsForm) {
+		String userId = "admin";
+		bankPayeeService.confirmPayee(payeeDetailsForm.getPayeeAccountNo(),
+				userId);
+		return NavigationConstant.CUSTOMER_PAGE
+				+ NavigationConstant.CUSTOMER_HOME_PAGE;
+	}
+
+	public String isPayeeExists() {
+
+		if (bankPayeeService.isPayeeExists("admin", "400")) {
+			System.out.println("Exists");
+		} else {
+			System.out.println("Does not exist");
+		}
+
+		return "";
+	}
+
 }
