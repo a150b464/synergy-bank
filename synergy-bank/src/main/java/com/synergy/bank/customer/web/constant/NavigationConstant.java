c5436862fd35f636df5c16e32f1c4a2342892f5d
jeffcng@gmail.com <jeffcng@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-19 18:17:42 +0000

    Mini Statement

--- synergy-bank/src/main/java/com/synergy/bank/customer/web/constant/NavigationConstant.java
+++ synergy-bank/src/main/java/com/synergy/bank/customer/web/constant/NavigationConstant.java
@@ -7,22 +7,22 @@ package com.synergy.bank.customer.web.constant;
  */
 public interface NavigationConstant {
 
-	final public static String COMMON_PAGE 				  = "WEB-INF/jsp/common/";
-	final public static String CUSTOMER_PAGE 			  = "WEB-INF/jsp/customer/";
-	final public static String ADMIN_PAGE 				  = "WEB-INF/jsp/admin/";
+	final public static String COMMON_PAGE = "WEB-INF/jsp/common/";
+	final public static String CUSTOMER_PAGE = "WEB-INF/jsp/customer/";
+	final public static String ADMIN_PAGE = "WEB-INF/jsp/admin/";
 	final public static String CUSTOMER_REGISTRATION_PAGE = "customerRegistration";
-	final public static String CUSTOMER_PAYEE_LIST_PAGE   = "payeeList";
-	final public static String ADD_PAYEE_PAGE 			  = "payeeDetailsSynergyBank";
-	final public static String LOGIN_PAGE 				  = "login";
-	final public static String CONFIRM_PAYEE_PAGE 		  = "payeeApprove";
-	final public static String CUSTOMER_HOME_PAGE 		  = "customerHome";
-	final public static String ADMIN_HOME_PAGE 			  = "adminHome";
-	final public static String FUND_TRANSFER_PAGE		  = "fundTransfer";
-	final public static String MAKE_PAYMENT_PAGE 	      = "selectPayee";
-	final public static String SELECT_PAYEE 			  = "selectPayee";
- 	final public static String CUSTOMER_INFORMATION		  = "customerDetailsTableView";
-	final public static String USER_SESSION_DATA 		  = "userSessionData";
-	final public static String CUSTOMER_ACCOUNT_SUMMARY   = "accountSummary";
-	final public static String CHANGE_PASSWORD_PAGE 	  = "changePassword";
-	final public static String VIEW_MINI_STATEMENT 		  = "viewMiniStatement";
+	final public static String CUSTOMER_PAYEE_LIST_PAGE = "payeeList";
+	final public static String ADD_PAYEE_PAGE = "payeeDetailsSynergyBank";
+	final public static String LOGIN_PAGE = "login";
+	final public static String CONFIRM_PAYEE_PAGE = "payeeApprove";
+	final public static String CUSTOMER_HOME_PAGE = "customerHome";
+	final public static String ADMIN_HOME_PAGE = "adminHome";
+	final public static String FUND_TRANSFER_PAGE = "fundTransfer";
+	final public static String MAKE_PAYMENT_PAGE = "selectPayee";
+	final public static String SELECT_PAYEE = "selectPayee";
+	final public static String CUSTOMER_INFORMATION = "customerDetailsTableView";
+	final public static String USER_SESSION_DATA = "userSessionData";
+	final public static String CUSTOMER_ACCOUNT_SUMMARY = "accountSummary";
+	final public static String CHANGE_PASSWORD_PAGE = "changePassword";
+	final public static String CUSTOMER_MINI_STATEMENT = "viewMiniStatement";
 }
