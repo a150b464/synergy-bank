cf23a8bd1e0f290f81a878b546fff382a4518d6e
ashish2chaudhari@gmail.com <ashish2chaudhari@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-22 16:56:29 +0000

    Implemented password functionality

---
+++ synergy-bank/src/main/java/com/synergy/bank/customer/service/impl/CustomerRegistrationQuestionsServiceImpl.java
@@ -0,0 +1,51 @@
+package com.synergy.bank.customer.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.synergy.bank.customer.dao.CustomerRegistrationQuestionsDao;
+import com.synergy.bank.customer.dao.entity.CustomerRegistrationQuestionsEntity;
+import com.synergy.bank.customer.service.CustomerRegistrationQuestionsService;
+import com.synergy.bank.customer.web.controller.form.CustomerRegistrationQuestionsForm;
+
+@Service("CustomerRegistrationQuestionsServiceImpl")
+@Scope("singleton")
+public class CustomerRegistrationQuestionsServiceImpl implements CustomerRegistrationQuestionsService {
+
+	
+	@Autowired
+	@Qualifier("CustomerRegistrationQuestionsDaoImpl")
+	private CustomerRegistrationQuestionsDao customerRegistrationQuestionsDao;
+	
+	@Override
+	public String addCustomerQuestionsAndAnswerByCustomerId(
+			CustomerRegistrationQuestionsForm questionForm) {
+		
+		CustomerRegistrationQuestionsEntity entity = new CustomerRegistrationQuestionsEntity();
+		BeanUtils.copyProperties(questionForm, entity);
+		customerRegistrationQuestionsDao.addCustomerQuestionsAndAnswerByCustomerId(entity);
+		
+		return "success";
+	}
+
+	@Override
+	public List<CustomerRegistrationQuestionsForm> findCustomerQuestionsAndAnswerByCustomerId(
+			String customerId) {
+			List<CustomerRegistrationQuestionsEntity> customerRegistrationQuestionsEntityList = customerRegistrationQuestionsDao.findCustomerQuestionsAndAnswerByCustomerId(customerId);
+			List<CustomerRegistrationQuestionsForm>   customerRegistrationQuestionsFormList   = new ArrayList<CustomerRegistrationQuestionsForm>(customerRegistrationQuestionsEntityList.size());
+
+			for(int i=0;i<customerRegistrationQuestionsEntityList.size();i++)
+			{
+				CustomerRegistrationQuestionsForm customerRegistrationQuestionsForm = new CustomerRegistrationQuestionsForm();	
+				BeanUtils.copyProperties(customerRegistrationQuestionsEntityList.get(i),customerRegistrationQuestionsForm);
+				customerRegistrationQuestionsFormList.add(customerRegistrationQuestionsForm);
+			}
+			return customerRegistrationQuestionsFormList;
+	}
+}
