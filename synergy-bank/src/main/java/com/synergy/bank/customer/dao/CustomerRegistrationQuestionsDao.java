17779d8d34962364cd36c841d078ec1317d88496
ashish2chaudhari@gmail.com <ashish2chaudhari@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-22 16:50:42 +0000

    Implemented password functionality

---
+++ synergy-bank/src/main/java/com/synergy/bank/customer/dao/CustomerRegistrationQuestionsDao.java
@@ -0,0 +1,10 @@
+package com.synergy.bank.customer.dao;
+
+import java.util.List;
+
+import com.synergy.bank.customer.dao.entity.CustomerRegistrationQuestionsEntity;
+
+public interface CustomerRegistrationQuestionsDao {
+	public abstract String addCustomerQuestionsAndAnswerByCustomerId(CustomerRegistrationQuestionsEntity questionEntity);
+	public abstract List<CustomerRegistrationQuestionsEntity> findCustomerQuestionsAndAnswerByCustomerId(String customerId);
+}
