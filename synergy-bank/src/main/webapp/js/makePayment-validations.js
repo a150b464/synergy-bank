877460fb942a0db37c1e0ad13333ab626f7c4be5
swapnil.joshi777@gmail.com <swapnil.joshi777@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-23 16:34:47 +0000

    Implemented AJAX runtime validation for addPayee, selectPayee, makePayments and customerRegistration Pages.

--- synergy-bank/src/main/webapp/js/makePayment-validations.js
+++ synergy-bank/src/main/webapp/js/makePayment-validations.js
@@ -7,10 +7,11 @@ function validateMakePaymentForm() {
 	var flag=true;
 	var transactionAmount = document.makePayments.transactionAmount.value;
 	//document.write('TRANSACTION AMOUNT is:' + transactionAmount);
+	alert(transactionAmount);
 
 	if(transactionAmount.length==0) {
+		alert("Transaction Amount cannot be blank."+transactionAmount);
 		flag=false;
-		alert("Transaction Amount cannot be blank.");
 		return flag;
 		
 	}
@@ -19,4 +20,5 @@ function validateMakePaymentForm() {
 		alert("The transactions details has been emailed to your given email address! Thank You for banking with us!!! "+transactionAmount);
 		document.makePayments.submit();		
 	}
+	return;
 }
--- synergy-bank/src/main/webapp/js/selectPayeeSubmitButton.js
+++ synergy-bank/src/main/webapp/js/selectPayeeSubmitButton.js
@@ -1,29 +1,25 @@
+/**
+ * 
+ * * @author Swapnil
+ */
+
+
 function  onSelectPayeeSubmit(action) {
-<<<<<<< .mine
 	
 	var action1="addPayeeAction";
 	var action2="makePaymentAction";
 	
-	//document.write("action name= "+ action);
 	
-	if(action==action1)
-=======
-		var action1="addPayeeAction";
-		var action2="makePaymentAction";
-		alert("action wwwwwwname= "+ action);
 		if(action==action1)
->>>>>>> .r144
+
 		{
-<<<<<<< .mine
 			document.forms[0].action="http://localhost:8080/synergy-bank/bank/addpayee.do"
-=======
-				//document.forms[0].action="http://localhost:8080/synergy-bank/bank/addPayee"
-			    alert("____(#_#+_#++___");
-				$("#customerTransactionCommand").attr("action","http://localhost:8080/synergy-bank/bank/addPayee");
->>>>>>> .r144
+				
+			//$("#customerTransactionCommand").attr("action","http://localhost:8080/synergy-bank/bank/addPayee");
 		}
 		
-		alert("Document wis going to be submitted");
-		$("#customerTransactionCommand").submit();
-		//document.forms[0].submit();	
+		
+		document.forms[0].submit();
+		//$("#customerTransactionCommand").submit();
+		return;	
 	}
