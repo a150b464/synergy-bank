023e88506963e278729187008889d7571c75c713
swapnil.joshi777@gmail.com <swapnil.joshi777@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-23 16:35:47 +0000

    Implemented AJAX runtime validation for addPayee, selectPayee, makePayments and customerRegistration Pages.

--- synergy-bank/src/main/java/com/synergy/bank/customer/web/controller/BankTransactionController.java
+++ synergy-bank/src/main/java/com/synergy/bank/customer/web/controller/BankTransactionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.synergy.bank.customer.service.BankPayeeService;
 import com.synergy.bank.customer.service.BankTransactionService;
@@ -25,6 +26,14 @@ public class BankTransactionController {
 	@Qualifier("BankPayeeServiceImpl")
 	private BankPayeeService bankPayeeService;
 
+	
+	// Add makePaymentsr Methods for Ajax Validation
+		@RequestMapping(value = "checkTransactionAmount.do", method = RequestMethod.GET)
+		public @ResponseBody String checkTransactionAmount(@RequestParam("ttransactionAmount") String transactionAmount) {
+		String result=bankTrasactionService.checkTransactionAmountNumber(transactionAmount);
+		  //@ResponseBody =>>by pass view resolver and write this data directly into the response body 
+	      return result;		
+		}
 	
 	@RequestMapping(value="makePayments",method=RequestMethod.GET) 
 	public String makePayment(Model model,
