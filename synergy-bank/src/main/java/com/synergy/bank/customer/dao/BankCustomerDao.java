9406c67e149ca8e2f4b4a25bdb067dd2e7cfb6ec
jeffcng@gmail.com <jeffcng@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-25 00:15:16 +0000

    Reset Password

--- synergy-bank/src/main/java/com/synergy/bank/customer/dao/BankCustomerDao.java
+++ synergy-bank/src/main/java/com/synergy/bank/customer/dao/BankCustomerDao.java
@@ -3,27 +3,42 @@ package com.synergy.bank.customer.dao;
 import java.util.List;
 
 import com.synergy.bank.customer.dao.entity.CustomerEntity;
+import com.synergy.bank.customer.dao.entity.CustomerRegistrationQuestionsEntity;
 import com.synergy.bank.customer.dao.entity.PayeeDetailsEntity;
 
 /**
  * @author this nagendra.yadav
- * @since 9th July 1014
- * This is contract for customer for the bank.
+ * @since 9th July 1014 This is contract for customer for the bank.
  */
 public interface BankCustomerDao {
 
 	public abstract String addCustomer(CustomerEntity entity);
+
 	public abstract String updateCustomer(CustomerEntity entity);
+
 	public abstract List<CustomerEntity> findCustomers();
-	public abstract List<CustomerEntity> findCustomerByColumnNameAndValue(String columnName, String value);
+
+	public abstract List<CustomerEntity> findCustomerByColumnNameAndValue(
+			String columnName, String value);
+
 	public abstract CustomerEntity findCustomerByUserId(String userid);
+
 	public abstract List<PayeeDetailsEntity> showPayeeListByUserId(String userId);
+
 	public abstract String deleteCustomer(CustomerEntity entity);
+
 	public abstract byte[] findPhotoById(String userId);
+
 	public abstract String addCustomerLoginDetails(CustomerEntity entity);
+
 	List<CustomerEntity> getCustomerListForRowNumbers(int initialRowNumber,
 			int maximumRowNumbers);
-	public abstract String updateCustomersnewLoginIdAndPassword(String userId, String password);
+
+	public abstract String updateCustomersnewLoginIdAndPassword(String userId,
+			String password);
+
 	int getCount();
-	
+
+	public List<CustomerRegistrationQuestionsEntity> findCustomerSecQuestions(
+			String userid);
 }
