a29121396244085f42d048b31114d0223a686c54
nagendra.yadav.j2ee <nagendra.yadav.j2ee@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-07-25 18:59:26 +0000

    soap fault implementation

--- synergy-bank/src/main/java/com/synergy/bank/soap/web/provider/BankCustomerSoapServiceProvider.java
+++ synergy-bank/src/main/java/com/synergy/bank/soap/web/provider/BankCustomerSoapServiceProvider.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Component;
 
 import com.synergy.bank.customer.service.BankCustomerService;
 import com.synergy.bank.customer.web.controller.form.CustomerForm;
+import com.synergy.bank.soap.web.provider.fault.CustomerNotFoundException;
+import com.synergy.bank.soap.web.provider.fault.SoapFaultMessage;
 import com.synergy.bank.soap.web.provider.wrapper.CustomerDetails;
 
 //name = is portType
@@ -34,22 +36,40 @@ public class BankCustomerSoapServiceProvider  implements BankCustomerSoapService
 	private BankCustomerService bankCustomerService;
 
 	@Override
-	public CustomerDetails findBankCustomers() {
+	public CustomerDetails findBankCustomers() throws CustomerNotFoundException {
 		if(logger.isDebugEnabled()){
 			logger.debug("___findBankCustomers web service method is invoked_____");
 		}
+		
 		List<CustomerForm> bankCustomers=bankCustomerService.findCustomers();
+		if(bankCustomers==null || bankCustomers.size()==0){
+			SoapFaultMessage faultMessage=new SoapFaultMessage();
+			faultMessage.setFaultCode("E1002");
+			faultMessage.setDetailMessage("Customers do not exist into the database, please contant to admin");
+			//first parameter = high level  description
+			CustomerNotFoundException customerNotFoundException=new CustomerNotFoundException("DB Issue",faultMessage);
+			throw customerNotFoundException;
+		}
+		
 		CustomerDetails customerDetails=new CustomerDetails();
 		customerDetails.setBankCustomers(bankCustomers);
 		return customerDetails;
 	}
 
 	@Override
-	public CustomerForm findBankCustomerByUserId(String customerUserId) {
+	public CustomerForm findBankCustomerByUserId(String customerUserId) throws CustomerNotFoundException {
 		if(logger.isDebugEnabled()){
 			logger.debug("___findBankCustomerByUserId web service method is invoked_____"+customerUserId);
 		}
 		CustomerForm customerForm=bankCustomerService.findCustomerByUserId(customerUserId);
+		if(customerForm==null || customerForm.getEmail()==null){
+			SoapFaultMessage faultMessage=new SoapFaultMessage();
+			faultMessage.setFaultCode("E1002");
+			faultMessage.setDetailMessage("Customers do not exist into the database, please contant to admin");
+			//first parameter = high level  description
+			CustomerNotFoundException customerNotFoundException=new CustomerNotFoundException("DB Issue",faultMessage);
+			throw customerNotFoundException;
+		}
 		return customerForm;
 	}
 	
