1650c8d32d110ea1f6ab04d5fa2cce47a8c617dc
nagendra.yadav.j2ee <nagendra.yadav.j2ee@64124627-2329-4ec4-c77c-1158b90f1d99>
2014-08-01 18:25:37 +0000

    spring and struts integration for loan module

---
+++ synergy-bank/src/main/java/com/synergy/bank/loan/service/CarLoanService.java
@@ -0,0 +1,14 @@
+package com.synergy.bank.loan.service;
+
+import com.synergy.bank.loan.web.action.form.CarForm;
+
+/**
+ * 
+ * @author nagendra.yadav
+ *
+ */
+public interface CarLoanService {
+	
+	public String applyforCarLoan(CarForm carForm);
+	
+}
---
+++ synergy-bank/src/main/java/com/synergy/bank/loan/web/action/form/CarForm.java
@@ -0,0 +1,101 @@
+package com.synergy.bank.loan.web.action.form;
+
+
+/**
+ * 
+ * @author nagendra
+ * 
+ */
+public class CarForm {
+	
+	private String carid;
+	private String color;
+	private String model;
+	private String vendor;
+	private String category;
+	private String email;
+	private String mobile;
+	private double price;
+	private byte[] image;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getCarid() {
+		return carid;
+	}
+
+	public void setCarid(String carid) {
+		this.carid = carid;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "CarForm [carid=" + carid + ", color=" + color + ", model="
+				+ model + ", vendor=" + vendor + ", category=" + category
+				+ ", email=" + email + ", mobile=" + mobile + ", price="
+				+ price + "]";
+	}
+
+}
