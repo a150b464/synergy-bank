550f3aeab8160b6e26429305f08dd69bc0f71230
nagendra.yadav.j2ee@gmail.com <nagendra.yadav.j2ee@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2015-01-08 18:14:25 +0000

    email sending..................

--- synergy-bank/src/main/java/com/synergy/bank/admin/web/constant/NavigationConstantAdmin.java
+++ synergy-bank/src/main/java/com/synergy/bank/admin/web/constant/NavigationConstantAdmin.java
@@ -12,6 +12,5 @@ public interface NavigationConstantAdmin {
 	final public static String PENDING_APPROVAL_CUSTOMER_LIST_PAGE="pendingApprovalCustomerList";
 	final public static String BLOCK_CUSTOMER_LIST_PAGE="blockCustomer";
 	final public static String ADMIN_ERROR_PAGE="adminErrorPage";
-	
 
 }
