a1162c3bb7ed2a9227ce123d72312de731111818
nagendra.yadav.j2ee@gmail.com <nagendra.yadav.j2ee@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2015-01-15 22:07:59 +0000

    Implementing Top Down Web Service for email functionality using metro framework

--- synergy-bank/src/main/java/com/synergy/bank/soap/web/provider/BankGallerySoapWebService.java
+++ synergy-bank/src/main/java/com/synergy/bank/soap/web/provider/BankGallerySoapWebService.java
@@ -9,10 +9,14 @@ import javax.imageio.ImageIO;
 import javax.jws.WebService;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.ContextLoader;
 
+import com.sun.org.apache.bcel.internal.generic.DMUL;
 import com.synergy.bank.admin.web.controller.form.GallaryPhotoForm;
 import com.synergy.bank.common.service.impl.GallaryService;
+import com.synergy.bank.soap.web.provider.fault.GalleryFaultMessage;
+import com.synergy.bank.soap.web.provider.fault.GalleryNotAvailableException;
 import com.synergy.bank.soap.web.provider.model.GallaryImageList;
 
 
@@ -32,11 +36,26 @@ public class BankGallerySoapWebService {
 	/* To use the spring code inside metro container, first call the spring container inside metro container */
 	
 
-	public GallaryImageList findGalleryImages() {
+	public void sendMessage(String firstName,String lastName){
+		System.out.println("firstName = "+firstName);
+		System.out.println("lastName = "+lastName);
+		
+	}
+	
+	public GallaryImageList findGalleryImages() throws GalleryNotAvailableException {
 		//fetching spring root web container........
 		ApplicationContext applicationContext=ContextLoader.getCurrentWebApplicationContext();
 		GallaryService gallaryService=(GallaryService)applicationContext.getBean("GallaryServiceImpl");
 		List<GallaryPhotoForm>  gallaryPhotoForms=gallaryService.findGalleryList();
+		if(gallaryPhotoForms!=null && gallaryPhotoForms.size()==0){
+			GalleryFaultMessage deMessage=new  GalleryFaultMessage();
+			deMessage.setDescription("Soap fault.");
+			deMessage.setDmessage("Gallary is not available in database , please contact to administrator.");
+			deMessage.setMessageCode("N1000");
+			deMessage.setServerip("http://192.168.10.7");
+			GalleryNotAvailableException availableException=new GalleryNotAvailableException("Gallery is empty",deMessage);
+		    throw availableException;
+		}
 		
 		for(GallaryPhotoForm form:gallaryPhotoForms){
 			String path=form.getPath();
