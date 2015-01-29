f8cb63b831d2b67d07368617b15ccade699c92ae
revanth.byalla@gmail.com <revanth.byalla@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2015-01-29 23:43:31 +0000

    SOAP Registration Provider Top Down

---
+++ synergy-bank/src/main/java/com/synergy/bank/registration/RegistrationInputVO.java
@@ -0,0 +1,569 @@
+
+package com.synergy.bank.registration;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for RegistrationInputVO complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="RegistrationInputVO">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="salutation" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="firstName" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="middleName" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="lastName" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="gender" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="maritalStatus" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="dob" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
+ *         &lt;element name="category" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="motherMaidenName" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="fatherName" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="email" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="mobile" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="ssn" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="occupation" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="occupationType" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="education" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="grossAnualIncome" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="sourceOfFunds" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="description" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "RegistrationInputVO", propOrder = {
+    "salutation",
+    "firstName",
+    "middleName",
+    "lastName",
+    "gender",
+    "maritalStatus",
+    "dob",
+    "category",
+    "motherMaidenName",
+    "fatherName",
+    "email",
+    "mobile",
+    "ssn",
+    "occupation",
+    "occupationType",
+    "education",
+    "grossAnualIncome",
+    "sourceOfFunds",
+    "description"
+})
+public class RegistrationInputVO {
+
+    @XmlElement(required = true)
+    protected String salutation;
+    @XmlElement(required = true)
+    protected String firstName;
+    @XmlElement(required = true)
+    protected String middleName;
+    @XmlElement(required = true)
+    protected String lastName;
+    @XmlElement(required = true)
+    protected String gender;
+    @XmlElement(required = true)
+    protected String maritalStatus;
+    @XmlElement(required = true)
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar dob;
+    @XmlElement(required = true)
+    protected String category;
+    @XmlElement(required = true)
+    protected String motherMaidenName;
+    @XmlElement(required = true)
+    protected String fatherName;
+    @XmlElement(required = true)
+    protected String email;
+    @XmlElement(required = true)
+    protected String mobile;
+    @XmlElement(required = true)
+    protected String ssn;
+    @XmlElement(required = true)
+    protected String occupation;
+    @XmlElement(required = true)
+    protected String occupationType;
+    @XmlElement(required = true)
+    protected String education;
+    @XmlElement(required = true)
+    protected String grossAnualIncome;
+    @XmlElement(required = true)
+    protected String sourceOfFunds;
+    @XmlElement(required = true)
+    protected String description;
+
+    /**
+     * Gets the value of the salutation property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSalutation() {
+        return salutation;
+    }
+
+    /**
+     * Sets the value of the salutation property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSalutation(String value) {
+        this.salutation = value;
+    }
+
+    /**
+     * Gets the value of the firstName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the value of the firstName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFirstName(String value) {
+        this.firstName = value;
+    }
+
+    /**
+     * Gets the value of the middleName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    /**
+     * Sets the value of the middleName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMiddleName(String value) {
+        this.middleName = value;
+    }
+
+    /**
+     * Gets the value of the lastName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets the value of the lastName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLastName(String value) {
+        this.lastName = value;
+    }
+
+    /**
+     * Gets the value of the gender property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * Sets the value of the gender property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setGender(String value) {
+        this.gender = value;
+    }
+
+    /**
+     * Gets the value of the maritalStatus property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    /**
+     * Sets the value of the maritalStatus property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMaritalStatus(String value) {
+        this.maritalStatus = value;
+    }
+
+    /**
+     * Gets the value of the dob property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDob() {
+        return dob;
+    }
+
+    /**
+     * Sets the value of the dob property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDob(XMLGregorianCalendar value) {
+        this.dob = value;
+    }
+
+    /**
+     * Gets the value of the category property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * Sets the value of the category property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCategory(String value) {
+        this.category = value;
+    }
+
+    /**
+     * Gets the value of the motherMaidenName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMotherMaidenName() {
+        return motherMaidenName;
+    }
+
+    /**
+     * Sets the value of the motherMaidenName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMotherMaidenName(String value) {
+        this.motherMaidenName = value;
+    }
+
+    /**
+     * Gets the value of the fatherName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    /**
+     * Sets the value of the fatherName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFatherName(String value) {
+        this.fatherName = value;
+    }
+
+    /**
+     * Gets the value of the email property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the value of the email property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    /**
+     * Gets the value of the mobile property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMobile() {
+        return mobile;
+    }
+
+    /**
+     * Sets the value of the mobile property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMobile(String value) {
+        this.mobile = value;
+    }
+
+    /**
+     * Gets the value of the ssn property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSsn() {
+        return ssn;
+    }
+
+    /**
+     * Sets the value of the ssn property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSsn(String value) {
+        this.ssn = value;
+    }
+
+    /**
+     * Gets the value of the occupation property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getOccupation() {
+        return occupation;
+    }
+
+    /**
+     * Sets the value of the occupation property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setOccupation(String value) {
+        this.occupation = value;
+    }
+
+    /**
+     * Gets the value of the occupationType property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getOccupationType() {
+        return occupationType;
+    }
+
+    /**
+     * Sets the value of the occupationType property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setOccupationType(String value) {
+        this.occupationType = value;
+    }
+
+    /**
+     * Gets the value of the education property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEducation() {
+        return education;
+    }
+
+    /**
+     * Sets the value of the education property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEducation(String value) {
+        this.education = value;
+    }
+
+    /**
+     * Gets the value of the grossAnualIncome property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getGrossAnualIncome() {
+        return grossAnualIncome;
+    }
+
+    /**
+     * Sets the value of the grossAnualIncome property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setGrossAnualIncome(String value) {
+        this.grossAnualIncome = value;
+    }
+
+    /**
+     * Gets the value of the sourceOfFunds property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSourceOfFunds() {
+        return sourceOfFunds;
+    }
+
+    /**
+     * Sets the value of the sourceOfFunds property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSourceOfFunds(String value) {
+        this.sourceOfFunds = value;
+    }
+
+    /**
+     * Gets the value of the description property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the value of the description property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+}
