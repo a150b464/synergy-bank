6b845287b2cb553a03fa35b3b735927be407b3d5
nagendra.yadav.j2ee@gmail.com <nagendra.yadav.j2ee@gmail.com@64124627-2329-4ec4-c77c-1158b90f1d99>
2015-04-23 23:12:57 +0000

    creating new customer home page using bootstrap library

---
+++ synergy-bank/src/main/java/com/synergy/bank/customer/web/constant/CustomerNavigationConstant.java
@@ -0,0 +1,14 @@
+package com.synergy.bank.customer.web.constant;
+
+
+/**
+ * 
+ * @author nagendra
+ *
+ */
+public interface CustomerNavigationConstant {
+	
+	final public static String CUSTOMER_PAGE = "WEB-INF/jsp/ncustomer/";
+	final public static String CUSTOMER_HOME_PAGE = "customerHome";
+	
+}
